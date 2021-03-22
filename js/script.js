022b70edf3726fc82ced317b6dc176a24f5905a7
sonalira <68660685+sonalira@users.noreply.github.com>
2021-03-23 00:08:33 +0530

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":655},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":655},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-page2","displayName":"page2","link":{"linkType":"LinkTypePage","href":"#!page-page2"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
